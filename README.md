ff2a0e9e1590d581d4a22921f1ba117779566dc6
JoPed17 <joao.pedrosil@hotmail.com>
2021-08-25 12:40:56 -0300

    att

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# Projeto-Portfolio
- 
+# portifolio
+
+Site desenvolvido de acordo com o tutorial do João Carlos https://www.youtube.com/watch?v=L_JSUkn7LME&list=PLM_90--7SomWgfPYCXnpuoY2L-Z_Z-AiV&ab_channel=Jo%C3%A3oCarlosLimaeSilva 
+
+## Linguagens
+
+Site feito em HTML, CSS e JavaScript
+
+
+
+
---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+);
+
+document.querySelector("#qtde").addEventListener("change", atualizarPreco)
+document.querySelector("#js").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-sim").addEventListener("change", atualizarPreco)
+document.querySelector("#layout-nao").addEventListener("change", atualizarPreco)
+document.querySelector("#prazo").addEventListener
+
+
